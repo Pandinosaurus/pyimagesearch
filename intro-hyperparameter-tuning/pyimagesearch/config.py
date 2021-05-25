71903466e0034cf4b651e6480f7f0a5d93c737c4
silvamfpedro <silva.mfpedro@gmail.com>
2021-05-25 15:22:30 +0100

    Added changes in config.py

--- intro-hyperparameter-tuning/pyimagesearch/config.py
+++ intro-hyperparameter-tuning/pyimagesearch/config.py
@@ -1,5 +1,5 @@
 # Specify the path to the dataset
-CSV_PATH = "abalone_train.csv"
+CSV_PATH = "abalone_dataset/abalone_train.csv"
 
 # Specify the column names of the dataframe
 COLS = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Age"]
